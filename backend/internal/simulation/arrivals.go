3b0f89c82a9e02e2b5b809837c28f1c7e5fcc43f
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-03-18 18:14:11 +0100

    feat: add func for turning arrivals into events

---
+++ backend/internal/simulation/arrivals.go
@@ -0,0 +1,29 @@
+package simulation
+
+import (
+	"time"
+)
+
+type arrivalGroup struct {
+	start    time.Duration
+	duration time.Duration
+	amount   int
+}
+
+// @param arrivals Slice of arrivalGroup, sorted by start.
+func ArrivalsToEvents(arrivals []arrivalGroup) []event {
+	events := make([]event, 0, len(arrivals))
+
+	for _, a := range arrivals {
+		// Calculate arrival times for all passengers, assuming uniform distribution.
+		for i := range a.amount {
+			t := a.duration.Nanoseconds() / int64(a.amount) * int64(i)
+			events = append(events, event{
+				time: a.start + time.Duration(t),
+				kind: eventArrival,
+			})
+		}
+	}
+
+	return events
+}
