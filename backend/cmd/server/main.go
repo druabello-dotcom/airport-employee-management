be81ef417292d8becad0c208fdb441e002575912
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-03-22 00:10:38 +0100

    fix: allow localhost:3000 on CORS

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -14,6 +14,8 @@ func main() {
 	mux.HandleFunc("GET /helloworld", handlers.HandleHelloWorld)
 	mux.HandleFunc("POST /checkpoints", handlers.HandleCheckpoints)
 
+	handler := handlers.WithCORS(mux)
+
 	log.Println("Server listening on port 8080")
-	log.Fatalln(http.ListenAndServe(":8080", mux))
+	log.Fatalln(http.ListenAndServe(":8080", handler))
 }
