93f91f86f0bb54bba274c83a303b833daee2f2f8
Julian Berg-Larsen <78998714+Yuqzii@users.noreply.github.com>
2026-03-23 08:20:15 +0100

    fix: rename chart dataset label to "Checkpoints"

--- frontend/src/updateChart.js
+++ frontend/src/updateChart.js
@@ -3,25 +3,26 @@ let chartExist = null;
 export function updateChart(dataObject) {
 	const times = [];
 	const checkpoints = [];
+
 	for (let i = 0; i < dataObject.length; i++) {
 		let totalMinutes = dataObject[i].time / (60 * Math.pow(10, 9));
 		let hour = Math.floor(totalMinutes / 60);
 		let minutes = totalMinutes % 60;
-		if (minutes < 10) 
+		if (minutes < 10)
 			times.push(`${hour}:0${minutes}`);
-		else 
+		else
 			times.push(`${hour}:${minutes}`);
 		checkpoints.push(dataObject[i].minOpen);
 	}
 
 	if (chartExist) chartExist.destroy();
-	chartExist  = new Chart("myChart", {
+	chartExist = new Chart("myChart", {
 		type: "line",
 		data: {
 			labels: times,
 			datasets: [
 				{
-					label: "Employee Managment",
+					label: "Checkpoints",
 					borderColor: "#2596be",
 					data: checkpoints,
 				}
