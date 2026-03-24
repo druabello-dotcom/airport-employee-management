c151c14ac11ca522bc5e04e264ea5300932b0802
Dru C. Romano Abello <druabello@gmail.com>
2026-03-24 13:48:18 +0100

    feat: style browse and upload file button

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,19 @@
-import { getNeededCheckpoints } from "./api.js";
+import { defaultConfig, getNeededCheckpoints } from "./api.js";
 import { updateChart } from "./updateChart.js";
 
 document.getElementById("uploadTrigger").addEventListener("click", () => {
 	document.getElementById("file").click();
 })
+
+document.getElementById("file").addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    const fileStatus = document.getElementById("file-name");
+    if (file) {
+        fileStatus.textContent = file.name;
+    } else {
+        fileStatus.textContent = "No file selected";
+    }
+});
 
 document.getElementById("form").addEventListener("submit", async event => {
 	event.preventDefault();
